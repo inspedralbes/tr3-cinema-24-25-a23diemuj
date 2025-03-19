8130074ac4354025a19a74bc4ad26bdda081ee03
a23diemuj <a23diemujper@inspedralbes.cat>
2025-03-19 19:31:43 +0100

    cm

--- front/src/comunication_manager.js
+++ front/src/comunication_manager.js
@@ -1,9 +1,9 @@
 
  // export const laravel = { URL: "http://a23diemujper.juego.daw.inspedralbes.cat/laravel/public/api" };
-export const laravel = { URL: "http://127.0.0.1:8000/api" };
+ export const laravel = { URL: "http://127.0.0.1:8000/api" };
  //export const laravel = { URL: "http://192.168.1.139:8000/api" };
 
- //export const laravel = { URL: "http://masket2.daw.inspedralbes.cat/laravel/public/api" };
+// export const laravel = { URL: "http://masket2.daw.inspedralbes.cat/laravel/public/api" };
 
 export async function register(param) {
     const URL = `${laravel.URL}/register`;
